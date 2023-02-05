ad320a423ed9759a7429b880a52bd5e7d71f7533
Jakub Madyda <jakub.madyda17@gmail.com>
2023-02-05 09:22:04 +0100

    finish calculator with the most importat currencies

---
+++ src/js/currencyCalculator.js
@@ -0,0 +1,55 @@
+import {currencies} from "./currencies.js";
+
+const curInputRef = document.getElementById('currencyInput');
+const quantityRef = document.getElementById('currencyQuantity');
+const curOutputRef = document.getElementById('currencyOutput');
+const optionRef = curInputRef.firstElementChild;
+const resultRef = document.getElementById('results')
+
+function generateOption({currencyName, flag, code}) {
+    const newOption = optionRef.cloneNode();
+    newOption.setAttribute('value', code);
+
+    const spanRef = document.createElement('span');
+    const spanContent = document.createTextNode(`${code} - ${currencyName}`);
+    spanRef.appendChild(spanContent);
+
+    newOption.appendChild(spanRef);
+
+    return newOption;
+}
+
+function generateOptions(selectRef, data) {
+    data.forEach((item) => {
+        selectRef.appendChild(generateOption(item));
+    })
+}
+
+generateOptions(curInputRef, currencies);
+generateOptions(curOutputRef, currencies);
+
+async function getCurrencyValue(curCode) {
+    try {
+        const response = await fetch(`http://api.nbp.pl/api/exchangerates/rates/a/${curCode}/`);
+        const data = await response.json()
+        return data.rates[0].mid;
+    } catch {
+        console.log('something is no yes')
+    }
+}
+
+async function calculateCurrencies(){
+    if(curInputRef.value !== '0' && curOutputRef.value !== 0 && quantityRef.value > '0'){
+        const curInput = await getCurrencyValue(curInputRef.value);
+        const curOutput = await getCurrencyValue(curOutputRef.value);
+        if(curInput && curOutput) {
+            resultRef.innerText = quantityRef.value * curInput / curOutput;
+        } else{
+
+        }
+    }
+}
+
+curInputRef.addEventListener('change', calculateCurrencies)
+quantityRef.addEventListener('change', calculateCurrencies)
+curOutputRef.addEventListener('change', calculateCurrencies)
